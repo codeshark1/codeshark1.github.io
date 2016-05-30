d3ead6c5897aca1ad228dd17a555b8981a7f6c91
codeshark <codeshark@yandex.ru>
2016-05-30 12:04:06 +0300

    update

--- dm/csbingo/js/main.js
+++ dm/csbingo/js/main.js
@@ -12,6 +12,29 @@ function tabs() {
     });
 }
 
+function chatExpand() {
+    $('#chatHeader').click(function(){
+        if ( $('#chatBody').is(':visible') ) {
+            $('#chatBody').slideUp().removeClass('open');
+            //$('#chatScroll').fadeOut();
+            //$('#chatScroll').css('visibility',0);
+        } else {
+            $('#chatScroll').height( $(window).height() - 130 );
+            $('#chatBody').slideDown().addClass('open');
+            //$('#chatScroll').fadeIn();
+            //$('#chatScroll').css('visibility',1);
+        }
+    });
+}
+
 $(document).ready(function(){
     tabs();
+    $('.b-carousel').slick({
+        infinite: false,
+        slidesToShow: 6,
+        appendArrows: $('.b-winners_carousel'),
+        prevArrow: "<button class='slick-prev'></button>",
+        nextArrow: "<button class='slick-next'></button>",
+    });   
+    chatExpand();
 });
