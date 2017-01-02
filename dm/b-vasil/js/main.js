09f0bb21800f67ad165478b0c34e9fb4f27bc1f6
codeshark1 <codeshark@yandex.ru>
2017-01-02 21:32:09 +0200

    vasil

---
+++ dm/b-vasil/js/main.js
@@ -0,0 +1,85 @@
+function responsiveIframe() {
+    $('iframe').each(function(){
+        var iw = $(this).width();
+        var ih = $(this).height();
+        var ip = $(this).parent().width();
+        var ipw = ip/iw;
+        var ipwh = Math.round(ih*ipw);
+        $(this).css({
+            'width': ip,
+            'height' : ipwh
+        });
+    });
+}
+
+function mainNav() {    
+    $('#nav-toggle').click(function(){
+        if ( $('#nav-main').is(':visible')){
+            $('#nav-main').slideUp();
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');
+            $('#nav-main').slideDown();
+        }
+    });
+}
+function categories() {
+    var categ_menu = $('#header .widget_categories');
+    $(categ_menu).find('.widget-header').click(function(){
+        if ( $(this).next('ul').is(':visible')){
+            $(this).next('ul').slideUp();
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');
+            $(this).next('ul').slideDown();
+        }
+    });
+}
+
+function up() {
+    $('#up').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });    
+}
+function but_up() {        
+    if ( $(window).scrollTop() > 100 ) {
+        $("#up").css('opacity',0.8);
+    } else {
+        $("#up").css('opacity',0);
+    }    
+}
+
+function clone_search() {
+    $('#searchform').clone().prependTo('.site-header .b-nav-wrap');
+}
+function clone_categs() {
+    $('.widget_categories').clone().appendTo('#header').addClass('visible-xs').removeClass('widget');
+}
+
+
+$(document).ready(function(){
+    responsiveIframe();
+    $(window).resize(function(){
+        responsiveIframe();
+    });
+    mainNav();
+    up(); 
+    but_up();
+    clone_search();
+    clone_categs();
+    categories();
+    $(window).scroll(function() {
+        but_up();        
+    });    
+});
